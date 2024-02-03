011f2bfd3c8164fe11deb285636fbf480bf890d4
Nathan <nmactal@seiunv.org>
2024-02-02 21:32:18 -0800

    uses proper $home variable in windows

--- script.bat
+++ script.bat
@@ -1,10 +1,10 @@
 @echo off
 
 rem Define the source directory where the files are located
-set "sourcedir=%~dp0"
+set "source_dir=%~dp0"
 
 rem Define the destination drietory where the files will be copied
-set "dest_dir=%USERPROFILE%\AppData\Roaming\.doom.d"
+set "dest_dir=%USERPROFILE%\.doom.d"
 
 rem Copy the files
 copy "%source_dir%config.el" "%dest_dir%\"
